7a7881a1b6ebc02e88d7a71cf064c518eab591df
DineshYerramothu <dyerramothu@gmail.com>
2022-12-21 17:18:02 -0600

    Update main.go
    Spelling correction

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	ArithmticOp "go/ArithmeticOp"
+	ArithmeticOp "go/ArithmeticOp"
 )
 
 func main() {
@@ -14,8 +14,8 @@ func main() {
 	fmt.Scanln(&num1)
 	fmt.Print("Enter num2 = ")
 	fmt.Scanln(&num2)
-	fmt.Println("Addition is: ", ArithmticOp.Sum(num1, num2))
-	fmt.Println("Subtraction is: ", ArithmticOp.Diff(num1, num2))
-	fmt.Println("Multiplication is: ", ArithmticOp.Multi(num1, num2))
-	fmt.Println("Division is: ", ArithmticOp.Div(num1, num2))
+	fmt.Println("Addition is: ", ArithmeticOp.Sum(num1, num2))
+	fmt.Println("Subtraction is: ", ArithmeticOp.Diff(num1, num2))
+	fmt.Println("Multiplication is: ", ArithmeticOp.Multi(num1, num2))
+	fmt.Println("Division is: ", ArithmeticOp.Div(num1, num2))
 }
